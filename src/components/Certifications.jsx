a63a78beaff48cd96756e76d159eb163ccb37536
Edwin Immanuel Damanik <edwindamanik5@gmail.com>
2023-01-14 15:36:18 +0700

    fix: add new file for certification content

---
+++ src/components/Certifications.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Certifications = () => {
+  return (
+    <div className="certification">
+      <h1>Certification</h1>
+      <div className="list-certification">
+        <p>
+          <i class="fa-solid fa-star"></i> React Developer & GraphQl Udemy
+        </p>
+        <p>
+          <i class="fa-solid fa-star"></i> Internship PT Javan Cipta Solusi
+        </p>
+        <p>
+          <i class="fa-solid fa-star"></i> Internship PT Liputan Enam dot Com
+        </p>
+        <p>
+          <i class="fa-solid fa-star"></i> Virtual Internship Investree
+        </p>
+        <p>
+          <i class="fa-solid fa-star"></i> Google IT Support Coursera
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Certifications;
